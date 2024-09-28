b771bed8892412e324018ae9bacf2e5402007b2c
Art <hakukonestudio@arify.fi>
2024-09-28 11:42:23 +0300

    eslint changed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function Login() {
     <div className="min-h-screen bg-gradient-to-b from-green-100 to-green-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Logi sisse EcoTrack'i
+          Logi sisse EcoTracki
         </h2>
       </div>
 
