6f670e96edc077ffe273a0e26977f5dea7a37816
Art <hakukonestudio@arify.fi>
2024-09-28 10:55:45 +0300

    new approach

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-// TypeScriptile tuleb lisada globaalne deklareerimine
 declare global {
-  var prisma: PrismaClient | undefined
+  let prisma: PrismaClient | undefined
 }
 
-// Kui oleme produktsioonikeskkonnas, loome uue Prisma kliendi,
-// arenduskeskkonnas kasutame globaalselt hoitavat instantsi.
 const prisma = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
